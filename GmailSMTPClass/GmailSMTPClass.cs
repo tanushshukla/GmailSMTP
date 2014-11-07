6ed753e24bb854004b1c236022b88719e47bb723
hellrokr <tanushdroid@gmail.com>
2014-11-07 12:42:39 +0530

    Initial Release
    Initial Release.

---
+++ GmailSMTPClass/GmailSMTPClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GmailSMTPClass
+{
+    public class GmailSMTPClass
+    {
+
+        private static void SendMail(string FromEmail, string ToEmail, string AuthenticationEmail, string AuthenticationEmailPassword, string Subject,string Body)
+        {
+            try
+            {
+
+                SmtpClient MailClient = new SmtpClient("aspmx.l.google.com", 25);
+                MailClient.EnableSsl = true;
+                MailClient.Credentials = new NetworkCredential(AuthenticationEmail, AuthenticationEmailPassword);
+                MailMessage Msg = new MailMessage();
+                Msg.From = new MailAddress(FromEmail);
+                Msg.To.Add(new MailAddress(ToEmail));
+                Msg.Subject = Subject;
+                Msg.Body = Body;
+
+                MailClient.Send(Msg);
+                Console.WriteLine("Sent");
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("failed to send email with the following error:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
